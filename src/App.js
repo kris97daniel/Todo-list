b6b83692081718b8c7db65874f4bfa865daf8314
Kristovan <daniel97kris@gmail.com>
2022-12-06 19:39:41 -0100

    list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import Form from './Components/Form';
+import TodoItem from './Components/TodoItem';
 
 function App() {
   return (
     <div className="App">
-      Hello, world!
+      <Form />
+      <TodoItem />
     </div>
   );
 }
---
+++ src/Components/Form.jsx
@@ -0,0 +1,13 @@
+import { Button, TextField } from '@mui/material'
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+        <Button variant="outlined">Outlined</Button>
+    </div>
+  )
+}
+
+export default Form
