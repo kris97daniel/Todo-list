f93d447ef08b50903b4f524ef94e8395dbbd387b
Kristovan <daniel97kris@gmail.com>
2022-12-29 12:11:28 -0100

    add todo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 //import logo from './logo.svg';
-//import './App.css';
-import Form from './Components/Form';
-import TodoItem from './Components/TodoItem';
+import './App.css';
+import React from 'react';
+//import Form from './Components/Form';
+//import TodoItem from './Components/TodoItem';
 
 function App() {
+  const [todos, setTodos] = React.useState([])
+  const [todo, setTodo] = React.useState("")
+
   return (
     <div className="App">
-      <Form />
-      <TodoItem />
+      <form>
+        <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo}/>
+        <button type="submit">Add Todo</button>
+      </form>
     </div>
   );
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,13 +1,13 @@
-import { Button, TextField } from '@mui/material'
-import React from 'react'
+//import { Button, TextField } from '@mui/material'
+//import React from 'react'
 
-function Form() {
-  return (
-    <div>
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-        <Button variant="outlined">Outlined</Button>
-    </div>
-  )
-}
+//function Form() {
+  //return (
+    //<div>
+      //  <TextField id="standard-basic" label="Standard" variant="standard" />
+        //<Button variant="outlined">Outlined</Button>
+//    </div>
+//  )
+//}
 
-export default Form
+//export default Form
